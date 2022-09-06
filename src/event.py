c7de90847618541004bdaee9e18a79a80ba43ff4
x07exploit <0x7ex@protonmail.com>
2022-09-06 14:44:58 -0400

    Pequeño cambio...

--- src/event.py
+++ src/event.py
@@ -20,9 +20,24 @@ class Event(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
+        embed = discord.Embed(
+            title = "Error",
+            color = discord.Color.red()
+        ).set_footer(
+            text = f"Pedido por: {ctx.author} | (ID: {ctx.author.id})", icon_url = ctx.author.display_avatar
+        )
+        
         if isinstance(error, commands.CommandNotFound):
-            await ctx.reply(f"> **Upss**, parece que el comando: `{ctx.invoked_with}` no existe.")  
-                  
+            embed.description = "El comando que has ingresado no existe."
+        elif isinstance(error, commands.MissingRequiredArgument):
+            embed.description = "Falta un argumento requerido."
+        elif isinstance(error, commands.MissingPermissions):
+            embed.description = "No tienes permisos para ejecutar este comando."
+        elif isinstance(error, commands.BotMissingPermissions):
+            embed.description = "No tengo permisos para ejecutar este comando."
+
+        await ctx.reply(embed = embed)
+
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Event(bot))
